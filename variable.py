c9ad6f453fb61b1f981b90e8fc4b7b0ff8ee874e
marijanedjalkova <nedyalkova.marya@gmail.com>
2016-04-07 17:46:32 +0100

    replaced inequality and equality constraints by a general expression constraint which takes an operator and evaluates the expression

--- variable.py
+++ variable.py
@@ -1,9 +1,14 @@
+import operator
+
 class Variable:
     
     def __init__(self, name, value, domain):
         self.name = name
         self.value = value
         self.domain = domain
+
+    def belongs(self, domain):
+    	return self.value >= domain.minValue and self.value <= domain.maxValue
 
 class Domain:
 
@@ -13,10 +18,33 @@ class Domain:
 
 class Constraint:
 
-	def __init__(self, variable, neededValue, operator):
+	def __init__(self, variable, allowedDomain):
 		self.variable = variable
-		self.neededValue = neededValue
-		self.operator = operator
+		self.allowedDomain = allowedDomain
+
+	def isSatisfied(self):
+		return variable.belongs(allowedDomain)
+
+
+class ExpressionConstraint(Constraint):
+	def __init__(self, variable1, variable2, op):
+		self.variable1 = variable1
+		self.variable2 = variable2
+		self.op = op
+
+	def function(op):
+	    return {
+	        '<' : operator.__lt__,
+	        '<=' : operator.__le__,
+	        '>' : operator.__gt__,
+	        '>=' : operator.__ge__,
+	        '==' : operator.__eq__,
+	        '!=' : operator.__ne__,
+	        }[op]
+
+	def isSatisfied(self):
+		return function(op)(variable1.value, variable2.value)
+
 
 class Model:
 	def __init__(self, variables):
